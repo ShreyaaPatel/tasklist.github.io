98bd89c107d3a969b586dfe3b2e0d262f3be12c8
ShreyaaPatel <144053241+ShreyaaPatel@users.noreply.github.com>
2024-01-19 15:24:58 +0530

    Drag function added

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from 'react';
+import ReactDragListView from 'react-drag-listview'
 import Form from './Form';
 
 
@@ -12,11 +13,6 @@ const Todo = () => {
   const [isEditItem, setIsEditItem] = useState(null);
   const [filter, setFilter] = useState(0);
   const [showForm, setShowForm] = useState(false);
-
-  // useEffect(() => {
-  //   const storedItems = JSON.parse(localStorage.getItem('tasks'));
-  //   setItems(storedItems);
-  // }, []);
   
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(items));
@@ -97,89 +93,109 @@ const Todo = () => {
     setShowForm(false);
   };
 
+  const dragProps = {
+    onDragEnd(fromIndex, toIndex) {
+      const newItemList = items.slice()
+      const item = newItemList.splice(fromIndex, 1)[0];
+      newItemList.splice(toIndex, 0, item);
+      setItems(newItemList)
+    },
+    nodeSelector: '#item',
+    handleSelector: '#handle'
+  };
+
   return (
-    <div className="container justify-content-center shadow p-3" style={{ minHeight:'100vh' }}>
-      <div className="row my-4">
-        <div className="text-center">
-          <h2 className='fw-semibold'>Task Manager</h2>
-        </div>
-      </div>     
-      
-      <Form
-        className={`mb-2  ${showForm ? '':'d-none'}`}
-        inputTitle={inputTitle}
-        setInputTitle={setInputTitle}
-        inputDesc={inputDesc}
-        setInputDesc={setInputDesc}
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-        isEditItem={isEditItem}
-        handleAdd={handleAdd}
-        handleSubmit={handleSubmit}
-        handleClose={handleClose}
-      /> 
+    <main className='bg-info'>
+      <div className="container bg-info-subtle justify-content-center shadow p-3" style={{ minHeight:'100vh' }}>
+        <div className="row my-4">
+          <div className="text-center">
+            <h2 className='fw-semibold'>Task Manager</h2>
+          </div>
+        </div>     
+        
+        <Form
+          className={`mb-2  ${showForm ? '':'d-none'}`}
+          inputTitle={inputTitle}
+          setInputTitle={setInputTitle}
+          inputDesc={inputDesc}
+          setInputDesc={setInputDesc}
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          isEditItem={isEditItem}
+          handleAdd={handleAdd}
+          handleSubmit={handleSubmit}
+          handleClose={handleClose}
+        /> 
 
-      <div className="row justify-content-between mb-4 px-0">
-        <div className={`col-md-auto ms-1 mb-2 mb-md-0 ${showForm ? 'd-none':''}` }>
-          <button className='btn btn-primary' onClick={() => setShowForm(true)}>Add Task</button>
-        </div>
-        <div className="col-md-auto me-1 text-end ms-auto">
-          <div className="btn-group" role="group">
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" onClick={() => setFilter(0)} checked={filter == 0}/>
-            <label className="btn btn-outline-info" for="btnradio1">All</label>
+        <div className="row justify-content-between mb-4 px-0">
+          <div className={`col-md-auto ms-1 mb-2 mb-md-0 ${showForm ? 'd-none':''}` }>
+            <button className='btn btn-primary' onClick={() => setShowForm(true)}>Add Task</button>
+          </div>
+          <div className="col-md-auto me-1 text-end ms-auto">
+            <div className="btn-group" role="group">
+              <input type="radio" className="btn-check" name="btnradio" id="btnradio1" onClick={() => setFilter(0)} checked={filter === 0}/>
+              <label className="btn btn-outline-primary" for="btnradio1">All</label>
 
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" onClick={() => setFilter(1)} checked={filter == 1} />
-            <label className="btn btn btn-outline-success" for="btnradio2">Completed</label>
+              <input type="radio" className="btn-check" name="btnradio" id="btnradio2" onClick={() => setFilter(1)} checked={filter === 1} />
+              <label className="btn btn btn-outline-success" for="btnradio2">Completed</label>
 
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio3" onClick={() => setFilter(-1)} checked={filter == -1} />
-            <label className="btn btn-outline-warning" for="btnradio3">Incomplete</label>
+              <input type="radio" className="btn-check" name="btnradio" id="btnradio3" onClick={() => setFilter(-1)} checked={filter === -1} />
+              <label className="btn btn-outline-warning" for="btnradio3">Incomplete</label>
+            </div>
           </div>
         </div>
+
+        <ReactDragListView {...dragProps}>
+        {items.map((elem) => (
+          <div id="item"
+            className={`d-flex rounded shadow-sm border mx-1 p-3 ps-2 mb-3 bg-gradient rounded bg-body
+              ${elem.status ? 'completed bg-secondary-subtle' : 'bg-body-subtle'}
+              ${(filter === 1) && !elem.status ? ' d-none':''} 
+              ${(filter === -1) && elem.status ? ' d-none':''}`
+            }
+            key={elem.id}
+          >
+            <div className='col-auto d-flex align-items-center me-2' id="handle">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#808080" className="bi bi-grip-vertical" viewBox="0 0 16 16">
+                <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+              </svg>
+            </div>
+            <div className="col">
+              <div className='row'>
+                <div className='fs-4'>{elem.name}</div>
+                <div className='mb-2'>{elem.desc}</div>
+              </div>
+              <div className="row justify-content-between align-items-center">
+                <div className='col-md mb-3 mb-md-0'>
+                  <span className=''>Date Added:</span> {elem.dateAdded}
+                </div>
+                <div className='col-md text-end'>
+                  <button
+                    className={`btn btn-sm ${elem.status ? 'btn-warning' : 'btn-success'}`}
+                    onClick={() => handleMarkCompleted(elem.id)}
+                  >
+                    {elem.status ? 'Mark Incomplete' : 'Mark Completed'}
+                  </button>
+                  <button
+                    className="btn btn-sm btn-primary mx-2"
+                    onClick={() => handleEdit(elem.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => handleDelete(elem.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        </ReactDragListView>
       </div>
-
-      {items.map((elem) => (
-        <div
-          className={`row rounded shadow-sm border mx-1 p-3 mb-3 bg-gradient rounded
-            ${elem.status ? 'completed bg-secondary-subtle' : 'bg-body-subtle'}
-            ${(filter == 1) && !elem.status ? ' d-none':''} 
-            ${(filter == -1) && elem.status ? ' d-none':''}`
-          }
-          key={elem.id}
-        >
-          <div className="col-12">
-            <div className='row'>
-              <div className='fs-4'>{elem.name}</div>
-              <div className='mb-2'>{elem.desc}</div>
-            </div>
-            <div className="row justify-content-between align-items-center">
-              <div className='col-md mb-3 mb-md-0'>
-                <span className=''>Date Added:</span> {elem.dateAdded}
-              </div>
-              <div className='col-md text-end'>
-                <button
-                  className={`btn btn-sm ${elem.status ? 'btn-warning' : 'btn-success'}`}
-                  onClick={() => handleMarkCompleted(elem.id)}
-                >
-                  {elem.status ? 'Mark Incomplete' : 'Mark Completed'}
-                </button>
-                <button
-                  className="btn btn-sm btn-primary mx-2"
-                  onClick={() => handleEdit(elem.id)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="btn btn-sm btn-danger"
-                  onClick={() => handleDelete(elem.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    </main>
   );
 };
 
